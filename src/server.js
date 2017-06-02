ed73140252682ff2dd351466113f8647b3d569f8
Akin909 <akin.sowemimo@gmail.com>
2017-06-02 15:23:03 +0100

    add css class for links, server and dynamic variable\

--- src/server.js
+++ src/server.js
@@ -11,17 +11,9 @@ app.use(express.static(path.join(__dirname, '..', 'public')));
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
-app.get('/:route', (req, res) => {
-  res.render('index', { route: req.params.route });
+app.get('/:route?', (req, res) => {
+  res.render('index', { route: req.params.route ? req.params.route : 'Home' });
 });
-
-//app.get('/favourites', (req, res) => {
-//res.render('index', { route: 'favourites' });
-//});
-
-//app.get('/add', (req, res) => {
-//res.render('index', { route: 'add' });
-//});
 
 app.listen(port, () => {
   console.log(`It all begins... on ${port}`);
